e1ba999b5667b0c05bc3142833291c8393e5738a
Reglament989 <52096394+Reglament989@users.noreply.github.com>
2020-05-09 18:05:09 +0200

    alfa 0.0.1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ ParentDir = os.getcwd()
 GitDirs = [x[0] for x in os.walk(os.path.join(ParentDir, '.git'))]
 MOD = 'TAR'
 PLATFORM = sys.platform
-OVER_SIZE = 1572864000 # 1,5 GB
+OVER_SIZE = 1048576000 # 1 GB
 
 if PLATFORM == 'win32' or PLATFORM == 'win64':
 	splitter = '\\'
@@ -32,34 +32,36 @@ def Compress(directory):
 	archives = [tar]
 	memory = 0
 	count = 0
-	# try:
-	for file in files:
-		if os.path.isdir(file):
-			continue
-	else:
-		absolute_path_file = os.path.join(directory, file)
-		file_size = os.path.getsize(absolute_path_file)
-		memory + file_size
-		if memory > OVER_SIZE:
-			count += 1
-			name_archive = os.path.join(os.getcwd(), directory.split(splitter)[-1] + f'_{count}' + '.tar.xz') 
-			archives[-1].close()
-			archives.append(tarfile.open(name_archive))
-		with open(absolute_path_file, 'rb') as f:
-			fileobj = f.read()
-		archives[-1].addfile(tarfile.TarInfo(file), fileobj)
-	# except Exception as e:
-	# 	raise e
-	# finally:
-	# 	try:
-	# 		for archive in archives:
-	# 			archive.close()
-	# 	except:
-	# 		pass
+	try:
+		for file in files:
+			if os.path.isdir(file):
+				continue
+		else:
+			absolute_path_file = os.path.join(directory, file)
+			file_size = os.path.getsize(absolute_path_file)
+			memory + file_size
+			if memory > OVER_SIZE:
+				count += 1
+				name_archive = os.path.join(os.getcwd(), directory.split(splitter)[-1] + f'_{count}' + '.tar.xz') 
+				archives[-1].close()
+				archives.append(tarfile.open(name_archive))
+			with open(absolute_path_file, 'rb') as f:
+				fileobj = f.read()
+			tarinfo = tarfile.TarInfo(file)
+			print(f"Append {absolute_path_file} to {archives[-1]}\n\tSize: {tarinfo.size}\n")
+			archives[-1].addfile(tarinfo, fileobj)
+	except Exception as e:
+		raise e
+	finally:
+		try:
+			for archive in archives:
+				archive.close()
+		except:
+			pass
 		
 
 # def main():
 # 	get_dirs()
 
 if __name__ == '__main__':
-  main()
+	main()
